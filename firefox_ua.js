875a1f84c2dc0ca26b6d66ec430f4e4873f1f373
lucasjohnston <developing@lucasco.de>
2023-02-16 23:13:43 +0000

    changes to make firefox work and allow page passthrough

--- firefox_ua.js
+++ firefox_ua.js
@@ -1,23 +1,19 @@
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  details => {
-    const { headers } = details
-    if (!headers) {
-      return undefined
-    }
-
-    return headers.map(header => {
-      if (header.name.toLowerCase() === 'user-agent') {
-        if (header.value?.includes('Mobile')) {
-          header.value =
-            'Mozilla/5.0 (Linux; Android 13; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36 EdgA/110.0.1587.41'
-          return header
-        }
+function rewriteUserAgentHeader(e) {
+  for (let header of e.requestHeaders) {
+    if (header.name.toLowerCase() === 'user-agent') {
+      if (header.value?.includes('Mobile')) {
         header.value =
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.100.0'
+          'Mozilla/5.0 (Linux; Android 13; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36 EdgA/110.0.1587.41'
       }
-      return header
-    })
-  },
-  { urls: ['*://*.bing.com/*'] },
+      header.value =
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.100.0'
+    }
+  }
+  return { requestHeaders: e.requestHeaders }
+}
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+  rewriteUserAgentHeader,
+  { urls: ['https://www.bing.com/*'] },
   ['blocking', 'requestHeaders'],
 )
--- syringe.js
+++ syringe.js
@@ -1,5 +1,33 @@
-window.addEventListener('load', async () => {
-  await new Promise(resolve => setTimeout(resolve, 50))
+let chatOpened = false
+
+const clickChat = async () => {
+  // Select menu item
   const chatBtn = document.getElementById('b-scopeListItem-conv')
+  if (
+    window.location.search.includes('&first') &&
+    (!window.location.search.includes('&first=1&') ||
+      (window.location.search.includes('&first=1') &&
+        window.location.search.substring(window.location.search.length - 1) ===
+          '1'))
+  ) {
+    console.log('Not on first page, not running')
+    return
+  }
+  if (!chatBtn || chatOpened === true) {
+    console.log('Chat button not found or already opened')
+    return
+  }
+
+  // Click item and wait
   chatBtn.firstChild.click()
-})
+  await new Promise(resolve => setTimeout(resolve, 50))
+
+  // See if chat is open
+  const sydneyChat = document.getElementsByTagName('cib-serp')
+  if (sydneyChat.length > 0) {
+    chatOpened = true
+  }
+  return
+}
+
+document.addEventListener('transitionrun', clickChat, true)
